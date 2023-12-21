f56f893d6e35b048930b9a19ce14c36271e2323a
Jerpharney <jerpharney@gmail.com>
2023-12-21 20:44:10 +0100

    log update

--- interfaces/discord/index.py
+++ interfaces/discord/index.py
@@ -1,6 +1,7 @@
 import discord
 import sys
 import os
+from datetime import datetime
 from discord import app_commands
 from discord.ext import commands
 
@@ -8,10 +9,16 @@ ROOT_DIR = os.path.abspath(os.curdir)
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 arguments = sys.argv
 TOKEN = arguments[1]
+sys.stderr = open(ROOT_DIR+"/logs/discord.txt", "a")
+
+def printToLog(message):
+    now = datetime.now().strftime("%d/%m/%Y %I:%M %p")
+    sys.stderr.write("[ "+now+" ] "+message+"\n")
+    sys.stderr.flush()
 
 @bot.event
 async def on_ready():
-    print('Loarby is ready.')
+    printToLog('Loarby is ready.')
     
 
 
@@ -20,7 +27,7 @@ async def sync(interaction: discord.Interaction):
     if interaction.user.id == 200459701752692738:
         await interaction.response.send_message('Synching commands...', ephemeral=True)
         await bot.tree.sync(guild=interaction.guild)
-        print("synched commands")
+        printToLog("synched commands")
     else:
         await interaction.response.send_message('You must be the owner to use this command!',  delete_after=5)
 
@@ -33,14 +40,15 @@ async def ping( interaction: discord.Interaction ):
 @bot.tree.command( name="hey", description="responds with hey") 
 async def hey( interaction: discord.Interaction ):
     await interaction.response.send_message('Hey!')
+    printToLog("hey")
 
 
 @bot.tree.command(name='shutdown', description='Owner only')
 async def shutdown(interaction: discord.Interaction):
     if interaction.user.id == 200459701752692738:
         await interaction.response.send_message('Shutting down...',  ephemeral=True)
-        print('Shutting down...')
-        sys.exit()
+        printToLog('Shutting down...')
+        await bot.close()
     else:
         await interaction.response.send_message('You must be the owner to use this command!')
 
